036962c9c21f443dd9d25d85e0c8ce623e3404ac
kinetas <kimjh7302@naver.com>
2024-10-21 21:28:23 +0900

    add

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,5 +2,4 @@
 
 # Node.js 애플리케이션 시작
 cd /home/ubuntu/nodetest  # 애플리케이션 디렉토리로 이동
-sudo pm2 start node --name node --interpreter=/usr/bin/node -- /home/ubuntu/nodetest/node.js
-pm2 list
+pm2 start node.js

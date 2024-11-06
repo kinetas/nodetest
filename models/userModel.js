10be11226bf55662bb844688bd76b31eb89c28c1
kinetas <kimjh7302@naver.com>
2024-11-06 21:17:53 +0900

    a

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,7 @@ const sequelize = require('../config/db');
 
 const User = sequelize.define('user', {
     u_id: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.STRING,
         primaryKey: true,
         autoIncrement: true
     },

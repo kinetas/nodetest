5b37b3d79be9d4bedee9235e919e327547a9682a
LeeJunMin <min73773@naver.com>
2024-11-07 22:59:01 +0900

    m

--- utils/passwordUtils.js
+++ utils/passwordUtils.js
@@ -1,19 +1,19 @@
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
-// // 비밀번호 암호화 함수
-// const hashPassword = async (password) => {
-//     const saltRounds = 10; // 솔트 라운드 수
-//     const hashedPassword = await bcrypt.hash(password, saltRounds);
-//     return hashedPassword;
-// };
+// 비밀번호 암호화 함수
+const hashPassword = async (password) => {
+    const saltRounds = 10; // 솔트 라운드 수
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    return hashedPassword;
+};
 
-// // 비밀번호 확인 함수
-// const comparePassword = async (password, hashedPassword) => {
-//     const isMatch = await bcrypt.compare(password, hashedPassword);
-//     return isMatch;
-// };
+// 비밀번호 확인 함수
+const comparePassword = async (password, hashedPassword) => {
+    const isMatch = await bcrypt.compare(password, hashedPassword);
+    return isMatch;
+};
 
-// module.exports = {
-//     hashPassword,
-//     comparePassword,
-// };
+module.exports = {
+    hashPassword,
+    comparePassword,
+};

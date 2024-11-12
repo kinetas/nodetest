6ddba8097a28b9ab83c6c2ae8e7ec7366fb0a011
kinetas <kimjh7302@naver.com>
2024-11-12 23:05:04 +0900

    a

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -15,7 +15,7 @@ exports.addRoom = async (req, res) => {
 
     try {
         await Room.create({ u1_id, u2_id, r_title: `${u1_id}-${u2_id}` });
-        await Room.create({ u2_id, u1_id, r_title: `${u1_id}-${u2_id}` });
+        await Room.create({ u1_id: u1_id, u2_id:u2_id, r_title: `${u2_id}-${u1_id}` });
         res.json({ message: '방이 성공적으로 추가되었습니다.' });
     } catch (error) {
         console.error(error); // 추가로 오류 로깅
@@ -28,7 +28,7 @@ exports.deleteRoom = async (req, res) => {
     const { u2_id } = req.params;
     try {
         await Room.destroy({ where: { u1_id, u2_id } });
-        await Room.destroy({ where: { u2_id, u1_id } });
+        await Room.destroy({ where: { u1_id:u2_id, u2_id:u1_id } });
         res.json({ message: '방이 성공적으로 삭제되었습니다.' });
     } catch (error) {
         res.status(500).json({ message: '방 삭제 중 오류가 발생했습니다.' });

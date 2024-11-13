9ec53b5aa53de541b252877c4021c49dc40a7645
병욱 조 <jbu0816@gmail.com>
2024-11-14 04:39:54 +0900

    BK

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -3,6 +3,18 @@ const db = require('../config/db');
 exports.createRoom = (socket, roomName) => {
   const roomId = Math.random().toString(36).substr(2, 9);
   socket.join(roomId);
+  
+  // 방 제목과 함께 room 테이블에 저장
+  db.query('INSERT INTO room (u1_id, u2_id, r_title, r_type) VALUES (?, ?, ?, ?)', 
+    [u1_id, u2_id, roomName, 'public'],  // 예시로 'public' 방 유형을 설정
+    (err, result) => {
+      if (err) {
+        console.error('Error creating room:', err);
+      } else {
+        console.log('Room created:', result);
+      }
+  });
+
   socket.emit('roomCreated', roomId);
 };
 
@@ -11,11 +23,20 @@ exports.joinRoom = (socket, { roomId, userId }) => {
   console.log(`User ${userId} joined room ${roomId}`);
 };
 
-exports.sendMessage = (io, socket, { roomId, message }) => {
+exports.sendMessage = (io, socket, { roomId, message, u1_id, u2_id }) => {
   io.to(roomId).emit('receiveMessage', message);
 
-  // 메시지를 데이터베이스에 저장
-  db.query('INSERT INTO messages (room_id, message) VALUES (?, ?)', [roomId, message], (err, result) => {
-    if (err) console.error('Error saving message:', err);
+  // 메시지를 r_message 테이블에 저장
+  const message_num = Math.random().toString(36).substr(2, 9);  // 메시지 번호 생성
+  const send_date = new Date().toISOString(); // 현재 시간
+  
+  db.query('INSERT INTO r_message (u1_id, u2_id, message_num, send_date, message_contents) VALUES (?, ?, ?, ?, ?)',
+    [u1_id, u2_id, message_num, send_date, message],
+    (err, result) => {
+      if (err) {
+        console.error('Error saving message:', err);
+      } else {
+        console.log('Message saved to r_message:', result);
+      }
   });
 };

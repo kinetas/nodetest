462cbb182fb1bd29884b6b56072880d2747576fb
LeeJunMin <min73773@naver.com>
2024-11-13 13:24:15 +0900

    signupAddRoom

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -10,17 +10,41 @@ exports.getRooms = async (req, res) => {
 };
 
 exports.addRoom = async (req, res) => {
-    const u1_id = req.session.user.id; // 세션에서 사용자 ID 가져오기
-    const { u2_id } = req.body;
+    // const u1_id = req.session.user.id; // 세션에서 사용자 ID 가져오기
+    // const { u2_id } = req.body;
 
-    try {
-        await Room.create({ u1_id, u2_id, r_title: `${u1_id}-${u2_id}` });
-        await Room.create({ u1_id: u2_id, u2_id:u1_id, r_title: `${u2_id}-${u1_id}` });
-        res.json({ message: '방이 성공적으로 추가되었습니다.' });
-    } catch (error) {
-        console.error(error); // 추가로 오류 로깅
-        res.status(500).json({ message: '방 추가 중 오류가 발생했습니다.' });
+    const { u2_id } = req.body;
+    // let u1_id;
+
+    // 세션에 저장된 사용자 ID가 있으면 u1_id에 할당, 없으면 u2_id와 동일하게 설정
+    if (req.session && req.session.user && req.session.user.id) {
+        const u1_id = req.session.user.id; // 세션에서 사용자 ID 가져오기
+        try {
+            await Room.create({ u1_id, u2_id, r_title: `${u1_id}-${u2_id}` });
+            await Room.create({ u1_id: u2_id, u2_id:u1_id, r_title: `${u2_id}-${u1_id}` });
+            res.json({ message: '방이 성공적으로 추가되었습니다.' });
+        } catch (error) {
+            console.error(error); // 추가로 오류 로깅
+            res.status(500).json({ message: '방 추가 중 오류가 발생했습니다.' });
+        }
+    } else {
+        try {
+            await Room.create({ u2_id, r_title: `${u2_id}` });
+            res.json({ message: '방이 성공적으로 추가되었습니다.' });
+        } catch (error) {
+            console.error(error); // 추가로 오류 로깅
+            res.status(500).json({ message: '방 추가 중 오류가 발생했습니다.' });
+        }
     }
+
+    // try {
+    //     await Room.create({ u1_id, u2_id, r_title: `${u1_id}-${u2_id}` });
+    //     await Room.create({ u1_id: u2_id, u2_id:u1_id, r_title: `${u2_id}-${u1_id}` });
+    //     res.json({ message: '방이 성공적으로 추가되었습니다.' });
+    // } catch (error) {
+    //     console.error(error); // 추가로 오류 로깅
+    //     res.status(500).json({ message: '방 추가 중 오류가 발생했습니다.' });
+    // }
 };
 
 exports.deleteRoom = async (req, res) => {

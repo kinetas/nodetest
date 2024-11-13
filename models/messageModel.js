ced913a32ac9a4d9a2ed5f61a3fde1d222f54999
kinetas <kimjh7302@naver.com>
2024-11-14 04:33:55 +0900

    a

---
+++ models/messageModel.js
@@ -0,0 +1,32 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+const RMessage = sequelize.define('RMessage', {
+    u1_id: {
+      type: DataTypes.STRING(20),
+      primaryKey: true,
+      allowNull: false,
+    },
+    u2_id: {
+      type: DataTypes.STRING(20),
+      primaryKey: true,
+      allowNull: false,
+    },
+    message_num: {
+      type: DataTypes.STRING(20),
+      primaryKey: true,
+      allowNull: false,
+    },
+    send_date: {
+      type: DataTypes.DATETIME,
+      allowNull: false,
+    },
+    message_contents: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+  }, {
+    tableName: 'r_message',
+    timestamps: false,
+  });
+  
+  module.exports = RMessage;

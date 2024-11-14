b4987d6aea44b016b0b7548b96c3403b634d82b8
LeeJunMin <min73773@naver.com>
2024-11-14 14:26:02 +0900

    findinfo

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,5 +6,6 @@ const findInfoController = require('../controllers/findInfoController');
 router.post('/login', authController.login);
 router.post('/register', authController.register);
 router.post('/findUid', findInfoController.findUid); // 아이디 찾기 경로 추가
+router.post('/changePassword', findInfoController.changePassword); // 비밀번호 변경 경로 추가
 
 module.exports = router;

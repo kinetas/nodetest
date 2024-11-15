1f575c3ef5f655dc2465048322b41b5a69158612
병욱 조 <jbu0816@gmail.com>
2024-11-16 02:21:39 +0900

    BK

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -3,7 +3,6 @@ const db = require('../config/db');
 exports.createRoom = (socket, roomName) => {
   const roomId = Math.random().toString(36).substr(2, 9);
   socket.join(roomId);
-  
   // 방 제목과 함께 room 테이블에 저장
   db.query('INSERT INTO room (u1_id, u2_id, r_title, r_type) VALUES (?, ?, ?, ?)', 
     [u1_id, u2_id, roomName, 'public'],  // 예시로 'public' 방 유형을 설정
@@ -29,7 +28,7 @@ exports.sendMessage = (io, socket, { roomId, message, u1_id, u2_id }) => {
 // 메시지를 r_message 테이블에 저장
 const message_num = Math.random().toString(36).substr(2, 9);  // 메시지 번호 생성
 const send_date = new Date().toISOString().slice(0, 19).replace('T', ' '); // 현재 시간
-  
+
 db.query('INSERT INTO r_message (u1_id, u2_id, r_id, message_num, send_date, message_contents) VALUES (?, ?, ?, ?, ?, ?)',
     [u1_id, u2_id, roomId, message_num, send_date, message], // 데이터 삽입
     (err, result) => {

d1bf706d576358a10c4ecb140856cc4204314d56
병욱 조 <jbu0816@gmail.com>
2024-11-15 16:24:23 +0900

    BK

--- socketServer.js
+++ socketServer.js
@@ -6,6 +6,7 @@ const socketIo = require('socket.io');
 //const fs = require('fs');
 const cors = require('cors');
 
+const chatController = require('./controllers/chatController');
 const db = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const chatRoutes = require('./routes/chatRoutes');

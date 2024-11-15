31e6ee914d2d9fb3513b965b5be87726ebfd31c1
병욱 조 <jbu0816@gmail.com>
2024-11-15 16:14:44 +0900

    BK

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,4 +1,5 @@
 const db = require('../config/db');
+const chatController = require('./controllers/chatController');
 
 exports.createRoom = (socket, roomName) => {
   const roomId = Math.random().toString(36).substr(2, 9);

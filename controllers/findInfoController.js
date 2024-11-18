359bb0d23c80fd8bbeab5dc3f5155d1265f06495
LeeJunMin <min73773@naver.com>
2024-11-19 02:06:02 +0900

    fi

--- controllers/findInfoController.js
+++ controllers/findInfoController.js
@@ -25,7 +25,7 @@ exports.findUid = async (req, res) => {
         }
     } catch (error) {
         console.error('사용자 조회 오류:', error);
-        res.status(500).json({ message: '서버 오류가 발생했습니다.' });
+        res.status(500).json({ message: `서버 오류 (${error})가 발생했습니다.` });
     }
 };
 

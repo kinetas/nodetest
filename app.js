ccbd45a4b3801a16aa2bc1c77f417783a936c41f
병욱 조 <jbu0816@gmail.com>
2024-11-19 07:03:57 +0900

    BK

--- app.js
+++ app.js
@@ -39,10 +39,10 @@ app.use(express.static('public'));
 
 // 메시지 저장을 처리하는 API 엔드포인트 추가
 app.post('/api/messages', (req, res) => {
-    const { message_contents, r_id, u1_id, u2_id } = req.body;
+    const { u1_id, u2_id, r_id, message_contents } = req.body;
     // DB에 메시지 저장 로직 추가
     if (!u1_id || !u2_id || !r_id || !message_contents) {
-        console.error('Missing required fields:', { message_contents, r_id, u1_id, u2_id  });
+        console.error('Missing required fields:', { u1_id, u2_id, r_id, message_contents});
         return res.status(400).json({ message: '필수 값이 누락되었습니다.' });
     }
     db.query(

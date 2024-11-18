e79e0849eb2ae8cd8275bdad8cdd43f966428a24
kinetas <kimjh7302@naver.com>
2024-11-18 21:43:44 +0900

    a

--- routes/cVoteRoutes.js
+++ routes/cVoteRoutes.js
@@ -6,5 +6,6 @@ router.get('/', cVoteController.getVotes); // 투표 리스트 가져오기
 router.post('/create', cVoteController.createVote); // 투표 생성
 router.post('/action', cVoteController.voteAction); // 투표 good/bad 업데이트
 router.get('/myVotes', cVoteController.getMyVotes); 
+router.delete('/delete/:c_number', cVoteController.deleteVote); // 투표 삭제 추가
 
 module.exports = router;

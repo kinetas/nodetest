1148ed2f9d27d3c7610fe0f170b2b6ba265c3898
병욱 조 <jbu0816@gmail.com>
2024-11-21 23:59:53 +0900

    BK

--- socketServer.js
+++ socketServer.js
@@ -140,7 +140,7 @@ io.on('connection', (socket) => {
 
     try {
       // 소켓 서버에서 API 서버로 HTTP 요청 전송
-      const response = await axios.post('http://13.124.126.234:3000/api/messages', {
+      const response = await axios.post('http://54.180.54.31:3000/api/messages', {
         message_contents,
         r_id,
         u1_id,

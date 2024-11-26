0436b57dcb50a21a618f03334aa3cad711e626ff
LeeJunMin <min73773@naver.com>
2024-11-26 12:29:18 +0900

    달성률

--- routes/resultRoutes.js
+++ routes/resultRoutes.js
@@ -1,21 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { getAchievementRates } = require('../controllers/resultController'); // resultController 가져오기
+const { getAchievementRates } = require('../controllers/resultController');
 
 // 사용자 달성률 API 라우트
 router.get('/achievement-rates', async (req, res) => {
     try {
-        const { u_id } = req.query; // 클라이언트에서 u_id 전달받기
-        if (!u_id) {
-            return res.status(400).json({ success: false, message: '사용자 ID가 필요합니다.' });
-        }
-
-        const result = await getAchievementRates(u_id);
+        const result = await getAchievementRates(req); // req 전달
 
         if (result.success) {
-            res.status(200).json(result); // 성공 시 달성률 데이터 반환
+            res.status(200).json(result); // 성공 시 데이터 반환
         } else {
-            res.status(500).json(result); // 오류 발생 시 오류 메시지 반환
+            res.status(400).json(result); // 실패 시 메시지 반환
         }
     } catch (error) {
         console.error('API 요청 처리 중 오류:', error);

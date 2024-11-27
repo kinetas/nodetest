6d94bef630f06e449d59d3afddc2bcc041ff8b21
LeeJunMin <min73773@naver.com>
2024-11-28 04:56:27 +0900

    미션 리스트 출력

--- controllers/missionController.js
+++ controllers/missionController.js
@@ -2,7 +2,7 @@
 const Mission = require('../models/missionModel'); // Mission 모델 불러오기
 // const { sequelize } = require('../models/missionModel'); // sequelize 객체 불러오기
 const Room = require('../models/roomModel'); // Room 모델 가져오기
-// const CRoom = require('../models/comunity_roomModel'); // Community Room 테이블
+const CRoom = require('../models/comunity_roomModel'); // Community Room 테이블
 const resultController = require('./resultController'); // resultController 가져오기
 const { v4: uuidv4, validate: uuidValidate } = require('uuid');
 const { Op } = require('sequelize'); // Sequelize의 연산자 가져오기

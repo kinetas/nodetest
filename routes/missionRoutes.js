0662a8eb5e81bd2709fcd4cb234b03de76c52745
LeeJunMin <min73773@naver.com>
2024-11-27 23:13:39 +0900

    mission

--- routes/missionRoutes.js
+++ routes/missionRoutes.js
@@ -1,17 +1,17 @@
 // routes/missionRoutes.js
 const express = require('express');
 const router = express.Router();
-const { getUserMissions, createMission, deleteMission, successMission, failureMission, printRoomMission  } = require('../controllers/missionController');
+const { getUserMissions, getAssignedMissions, getCreatedMissions, createMission, deleteMission, successMission, failureMission, printRoomMission  } = require('../controllers/missionController');
 const requireAuth = require('../middleware/authMiddleware'); // requireAuth 미들웨어 경로 확인
 
 // 미션 리스트 반환 라우트
 router.get('/missions', requireAuth, getUserMissions);
 
-// // 자신이 수행해야 할 미션
-// router.get('/missions/assigned', requireAuth, getAssignedMissions);
+// 자신이 수행해야 할 미션
+router.get('/missions/assigned', requireAuth, getAssignedMissions);
 
-// // 자신이 부여한 미션
-// router.get('/missions/created', requireAuth, getCreatedMissions);
+// 자신이 부여한 미션
+router.get('/missions/created', requireAuth, getCreatedMissions);
 
 // 미션 생성 요청 처리
 router.post('/missioncreate', requireAuth, createMission);

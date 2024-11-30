7fdb6dc68786025ac5a0158d8fb1b3df42c2a8b6
병욱 조 <jbu0816@gmail.com>
2024-12-01 03:04:09 +0900

    BK

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -16,4 +16,14 @@ router.post('/join-room', authenticateToken, chatController.joinRoom);
 // 채팅방에 사진 및 메시지 업로드
 router.post('/send-message', authenticateToken, upload.single('file'), chatController.sendMessageWithFile);
 
+router.get('/messages/:r_id', async (req, res) => {
+    const { r_id } = req.params;
+    try {
+        const messages = await chatController.getMessages(r_id);
+        res.json(messages);
+    } catch (error) {
+        console.error('Error fetching messages:', error);
+        res.status(500).json({ error: 'Failed to fetch messages' });
+    }
+});
 module.exports = router;

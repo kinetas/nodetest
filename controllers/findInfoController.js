fb6c40148165969c735492f2e20c23edebf25c8c
LeeJunMin <min73773@naver.com>
2024-11-30 23:19:29 +0900

    아이디찾기/비밀번호변경

--- controllers/findInfoController.js
+++ controllers/findInfoController.js
@@ -21,8 +21,10 @@ exports.findUid = async (req, res) => {
 
         // 사용자가 존재하면 u_id를 응답, 없으면 오류 메시지
         if (user) {
+            console.log(JSON.stringify({ userId: user.u_id }));
             return res.status(200).json({ userId: user.u_id });
         } else {
+            console.log(JSON.stringify({ message: '일치하는 사용자가 없습니다.' }));
             return res.status(404).json({ message: '일치하는 사용자가 없습니다.' });
         }
     } catch (error) {
@@ -47,8 +49,10 @@ exports.changePassword = async (req, res) => {
 
         // 업데이트 성공 시 응답
         if (result[0] > 0) {
+            console.log(JSON.stringify({ message: '비밀번호가 성공적으로 변경되었습니다.' }));
             return res.status(200).json({ message: '비밀번호가 성공적으로 변경되었습니다.' });
         } else {
+            console.log(JSON.stringify({ message: '사용자를 찾을 수 없습니다.' }));
             return res.status(404).json({ message: '사용자를 찾을 수 없습니다.' });
         }
     } catch (error) {

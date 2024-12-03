1c38d05dc03b6975e0da593e0927a065fd454dcd
병욱 조 <jbu0816@gmail.com>
2024-12-04 06:26:24 +0900

    BK

--- config/FCM.js
+++ config/FCM.js
@@ -15,6 +15,7 @@ const serviceAccount = {
     token_uri: process.env.FIREBASE_TOKEN_URI,
     auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
     client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+    universe_domain:process.env.FIREBASE_UNIVERSE_DOMAIN
 };
 
 admin.initializeApp({

1b47bd1e57cf5cb1c9698e4892da31d4c02f8118
LeeJunMin <min73773@naver.com>
2024-12-03 23:04:22 +0900

    커뮤니티미션컨트롤러

--- controllers/c_missionController.js
+++ controllers/c_missionController.js
@@ -324,8 +324,8 @@ exports.checkMissionStatus = async () => {
                         await Room.destroy({ where: { u1_id: room.u2_id, u2_id: room.u_id } });
                         await Room.destroy({ where: { u1_id: room.u_id, u2_id: room.u2_id } });
                         // await CRoom.destroy({ where: { cr_num: room.cr_num } });
-                        await CRoom.destroy({ where: { u1_id: room.u2_id, u2_id: room.u_id } });
-                        await CRoom.destroy({ where: { u1_id: room.u_id, u2_id: room.u2_id } });
+                        await CRoom.destroy({ where: { u_id: room.u2_id, u2_id: room.u_id } });
+                        await CRoom.destroy({ where: { u_id: room.u_id, u2_id: room.u2_id } });
                     }
                 }
             }

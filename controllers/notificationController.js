5fbc5ed6cf40a350875d7a14246587f91bc2d8ab
병욱 조 <jbu0816@gmail.com>
2024-12-04 07:04:34 +0900

    BK

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,11 +1,12 @@
 const { sendNotification } = require('../config/FCM');
 
-// Express 요청을 처리하고 sendNotification 호출
+// Express 요청을 처리하고 sendNotification 호출(클라이언트로부터 필수값 검증)
 const sendNotificationController = async (req, res) => {
     const { token, title, body } = req.body;
 
     if (!token || !title || !body) {
         return res.status(400).json({ message: 'FCM 토큰, 제목, 내용이 필요합니다.' });
+        console.log('Request received:', req.body);
     }
 
     try {

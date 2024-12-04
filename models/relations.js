3c715893b476501ce089cd3bca1e68f70b8d8c0f
LeeJunMin <min73773@naver.com>
2024-12-05 03:35:01 +0900

    미션

--- models/relations.js
+++ models/relations.js
@@ -2,7 +2,7 @@ const Room = require('./roomModel');
 const Mission = require('./missionModel');
 
 // Room과 Mission 간의 관계 설정
-Room.hasMany(Mission, { foreignKey: 'r_id', as: 'missions' });
 Mission.belongsTo(Room, { foreignKey: 'r_id', as: 'room' });
+Room.hasMany(Mission, { foreignKey: 'r_id', as: 'missions' });
 
 module.exports = { Room, Mission };

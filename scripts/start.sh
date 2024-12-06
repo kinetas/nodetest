e7c3b218a9886c9d0fa54c2b458f2c4ad8a630c7
kinetas <kimjh7302@naver.com>
2024-12-07 03:33:22 +0900

    a

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# Node.js ���ø����̼� ����
-cd /home/ubuntu/nodetest  # ���ø����̼� ���丮�� �̵�
+# JSON 파일에서 \n을 실제 줄바꿈으로 변환
+JSON_FILE="/home/ubuntu/nodetest/firebase-adminsdk.json"
+
+if [ -f "$JSON_FILE" ]; then
+  sed -i 's/\\n/\n/g' "$JSON_FILE"
+  echo "Updated JSON file with line breaks."
+else
+  echo "JSON file not found: $JSON_FILE"
+fi
+
+# Node.js 애플리케이션 시작
+cd /home/ubuntu/nodetest  # 애플리케이션 디렉토리로 이동
 pm2 start app.js
 pm2 start socketServer.js
 pm2 save

aae0d557b8fffd360345b58804afe70f56cb642d
kinetas <kimjh7302@naver.com>
2024-12-06 23:32:22 +0900

    A

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # ���� ���� ����
 # rm -rf /home/ubuntu/nodetest/*
-find /home/ubuntu/nodetest/ -mindepth 1 ! -name "firebase-adminsdk.json" -exec rm -rf {} +
+if [ -d "/home/ubuntu/nodetest/" ]; then
+  find /home/ubuntu/nodetest/ -mindepth 1 ! -name "firebase-adminsdk.json" -exec rm -rf {} + 2>/dev/null
+else
+  echo "Directory /home/ubuntu/nodetest/ does not exist. Skipping cleanup."
+fi

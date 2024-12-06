1744f8063c8f42d6fc11764fce6cac9011d2d4fa
kinetas <kimjh7302@naver.com>
2024-12-06 23:42:28 +0900

    A

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,7 +2,7 @@
 # ���� ���� ����
 # rm -rf /home/ubuntu/nodetest/*
 if [ -d "/home/ubuntu/nodetest/" ]; then
-  # firebase-adminsdk.json과 .env 파일 제외 삭제
+  # .env 및 firebase-adminsdk.json은 유지, 하지만 나중에 업데이트될 경우 덮어쓰기
   find /home/ubuntu/nodetest/ -mindepth 1 ! -name "firebase-adminsdk.json" ! -name ".env" -exec rm -rf {} + 2>/dev/null
 else
   echo "Directory /home/ubuntu/nodetest/ does not exist. Skipping cleanup."

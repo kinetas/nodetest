6938665d91c406c943bffd56b68311be22ed368e
병욱 조 <jbu0816@gmail.com>
2024-12-10 02:50:41 +0900

    채팅방 정상화

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -7,11 +7,11 @@ const authenticateToken = require('../auth');
 const storage = multer.memoryStorage(); // 파일을 메모리에 저장
 const upload = multer({ storage });
 
-// // 채팅방 생성
-// router.post('/create-room', authenticateToken, chatController.createRoom);
+// 채팅방 생성
+router.post('/create-room', authenticateToken, chatController.createRoom);
 
-// // 채팅방 참여
-// router.post('/join-room', authenticateToken, chatController.joinRoom);
+// 채팅방 참여
+router.post('/join-room', authenticateToken, chatController.joinRoom);
 
  // 채팅방에 사진 및 메시지 업로드
 router.post('/send-message', authenticateToken, upload.single('file'), chatController.sendMessageWithFile);

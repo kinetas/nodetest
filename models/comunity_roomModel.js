0c0b500f3097a2f6c60775d0d9c1a33606cb87a6
LeeJunMin <min73773@naver.com>
2024-12-09 10:54:13 +0900

    커뮤니티 미션 수정 - 미션내용, 마감기한

--- models/comunity_roomModel.js
+++ models/comunity_roomModel.js
@@ -32,6 +32,14 @@ const CRoom = sequelize.define('CRoom', {
         type: DataTypes.STRING(5),
         allowNull: true,
     },
+    contents: {
+        type: DataTypes.STRING(500),
+        allowNull: true,
+    },
+    deadline: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    },
 }, {
     tableName: 'community_room', // 테이블 이름 (DB 테이블 이름과 매칭)
     timestamps: false, // createdAt, updatedAt 컬럼 사용 안 함

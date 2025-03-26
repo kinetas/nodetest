517ed177021239e50b4c7fd55b69936447777c08
kinetas <kimjh7302@naver.com>
2025-03-26 20:59:42 +0900

    a

--- rag_server/main.py
+++ rag_server/main.py
@@ -7,7 +7,7 @@ from langchain.chains import RetrievalQA
 
 app = FastAPI()
 
-embedding = OllamaEmbeddings()
+embedding = OllamaEmbeddings(model="llama3")  # 모델 명을 추가
 db = Chroma(persist_directory="db", embedding_function=embedding)
 retriever = db.as_retriever()
 
@@ -23,5 +23,4 @@ async def recommend(req: RAGRequest):
     query = f"{req.category} 운동 좋아하는 사람에게 오늘 할 만한 운동 추천해줘"
     response = qa.run(query)
     return {"message": response}
-
 

9ea1eb074aba0d1aa311a7d525ab22d82c26f28f
kinetas <kimjh7302@naver.com>
2025-03-26 22:54:39 +0900

    a

--- rag_server/dataPush.py
+++ rag_server/dataPush.py
@@ -1,7 +1,7 @@
 import hashlib
 import os
 from langchain_community.document_loaders import TextLoader
-from langchain_community.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import CharacterTextSplitter
 from langchain_ollama import OllamaEmbeddings
 from langchain_chroma import Chroma
 

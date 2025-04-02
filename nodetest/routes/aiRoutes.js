3323a246d0f388d32d7626715eb3a5ea5e85d59a
LeeJunMin <min73773@naver.com>
2025-04-02 18:33:06 +0900

    ai.html, aiRoutes.js ai관련수정

--- nodetest/routes/aiRoutes.js
+++ nodetest/routes/aiRoutes.js
@@ -6,19 +6,19 @@ const aiController = require('../controllers/aiController');
 // FastAPI 주소
 const RAG_SERVER_URL = 'http://rag_server:8000/recommend'; // Docker 내부 주소
 
-router.post('/recommend', async (req, res) => {
-    const { message } = req.body;
+// router.post('/recommend', async (req, res) => {
+//     const { message } = req.body;
 
-    try {
-        const response = await axios.post(RAG_SERVER_URL, {
-            category: message
-        });
-        return res.json({ result: response.data.message });
-    } catch (error) {
-        console.error('RAG 서버 요청 실패:', error.message);
-        return res.status(500).json({ error: '추천 요청 실패' });
-    }
-});
+//     try {
+//         const response = await axios.post(RAG_SERVER_URL, {
+//             category: message
+//         });
+//         return res.json({ result: response.data.message });
+//     } catch (error) {
+//         console.error('RAG 서버 요청 실패:', error.message);
+//         return res.status(500).json({ error: '추천 요청 실패' });
+//     }
+// });
 
 router.post('/ask', aiController.askQuestion);
 

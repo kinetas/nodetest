f698b6dacf8fd41f29d10028e23bfd203de012c7
kinetas <kimjh7302@naver.com>
2025-04-02 18:30:42 +0900

    index

--- webRTC_server/main.py
+++ webRTC_server/main.py
@@ -5,13 +5,9 @@ import json
 import os
 
 app = FastAPI()
-# 정적 파일 static 폴더 경로 설정
-    app.mount("/", StaticFiles(directory="static", html=True), name="static")
-
 
 # WebSocket 연결 저장소 설정
 active_connections: List[WebSocket] = []
-
 
 # WebSocket 엔드포인트
 @app.websocket("/ws")
@@ -30,3 +26,9 @@ async def websocket_endpoint(websocket: WebSocket):
     except Exception:
         active_connections.remove(websocket)
         
+# 정적 파일 static 폴더 경로 설정
+    app.mount("/static", StaticFiles(directory="static", html=True), name="static")
+
+@app.get("/")
+def serve_index():
+    return FileResponse("static/index.html")

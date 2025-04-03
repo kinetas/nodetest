d3114186c9849cec5d70ef95407f7ba612f33d74
kinetas <kimjh7302@naver.com>
2025-04-03 18:49:55 +0900

    a

--- nodetest/routes/aiRoutes.js
+++ nodetest/routes/aiRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const aiController = require('../controllers/aiController');
+const aiController = require('../controllers/AIController');
 
 // // FastAPI 주소
 // const RAG_SERVER_URL = 'http://rag_server:8000/recommend'; // Docker 내부 주소

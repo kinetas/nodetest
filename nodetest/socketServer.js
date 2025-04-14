df30cd1b652cf920b576002aa38ee929276418bf
LeeJunMin <min73773@naver.com>
2025-04-14 20:57:01 +0900

    소켓서버 수정

--- nodetest/socketServer.js
+++ nodetest/socketServer.js
@@ -29,8 +29,6 @@ const jwt = require('jsonwebtoken');
 const secretKey = process.env.JWT_SECRET_KEY || 'your_secret_key';
 
 
-const axios = require('axios');
-
 let keycloakPublicKey = null;
 
 function convertCertToPEM(cert) {

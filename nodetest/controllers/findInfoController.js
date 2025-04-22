737124c624f56d663cda60b8913e5bf23bdf9c9f
LeeJunMin <min73773@naver.com>
2025-04-22 16:39:05 +0900

    findInfoController.js 수정

--- nodetest/controllers/findInfoController.js
+++ nodetest/controllers/findInfoController.js
@@ -138,11 +138,11 @@ exports.changePassword = async (req, res) => {
             console.log(JSON.stringify({ message: '비밀번호가 성공적으로 변경되었습니다.' }));
             return res.status(200).json({ message: '비밀번호가 성공적으로 변경되었습니다.' });
         } else {
-            console.log(JSON.stringify({ message: '사용자를 찾을 수 없습니다.' }));
-            return res.status(404).json({ message: '사용자를 찾을 수 없습니다.' });
+            console.log(JSON.stringify({ message: '사용자를 찾을 수 없습니다.(findInfoController.js:141)' }));
+            return res.status(404).json({ message: '사용자를 찾을 수 없습니다.(findInfoController.js:142)' });
         }
     } catch (error) {
-        console.error('비밀번호 변경 오류:', error);
-        res.status(500).json({ message: '서버 오류가 발생했습니다.' });
+        console.error('비밀번호 변경 오류(findInfoController.js:145):', error);
+        res.status(500).json({ message: '서버 오류가 발생했습니다.(findInfoController.js:146)' });
     }
 };

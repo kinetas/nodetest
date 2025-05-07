9de8f8a1d1afc9ef161c0fcd7b90411e7f3115bf
LeeJunMin <min73773@naver.com>
2025-05-07 19:57:50 +0900

    comunity_roomModel.js image 추가 in nodetest

--- nodetest/models/comunity_roomModel.js
+++ nodetest/models/comunity_roomModel.js
@@ -63,6 +63,10 @@ const CRoom = sequelize.define('CRoom', {
         type: DataTypes.DATE,
         allowNull: true,
     },
+    image: {
+        type: DataTypes.BLOB('long'),
+        allowNull: true,
+    },
 }, {
     tableName: 'community_room', // 테이블 이름 (DB 테이블 이름과 매칭)
     timestamps: false, // createdAt, updatedAt 컬럼 사용 안 함

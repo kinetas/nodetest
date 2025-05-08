cb72031a57c5cc427c6d070126eb6e0097edce7f
LeeJunMin <min73773@naver.com>
2025-05-08 11:39:30 +0900

    c_missionRoutes.js 수정 in nodetest

--- nodetest/routes/c_missionRoutes.js
+++ nodetest/routes/c_missionRoutes.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { createCommunityMission, acceptCommunityMission, deleteCommunityMission, getCommunityMission } = require('../controllers/c_missionController');
+const c_missionController = require('../controllers/c_missionController');
 const loginRequired = require('../middleware/loginRequired'); // JWT 기반 인증 미들웨어
 
-router.post('/create', loginRequired, createCommunityMission);
-router.post('/accept', loginRequired, acceptCommunityMission);
-router.delete('/delete', loginRequired, deleteCommunityMission);
+router.post('/create', loginRequired, c_missionController.createCommunityMission);
+router.post('/accept', loginRequired, c_missionController.acceptCommunityMission);
+router.delete('/delete', loginRequired, c_missionController.deleteCommunityMission);
 
-router.get('/list', loginRequired, getCommunityMission);
+router.get('/list', loginRequired, c_missionController.getCommunityMission);
 
-router.post('/createGeneralCommunity', loginRequired, createCommunity);
-router.get('/printGeneralCommunityList', loginRequired, printGeneralCommunity);
-router.post('/recommendCommunity', loginRequired, recommendCommunity);
+router.post('/createGeneralCommunity', loginRequired, c_missionController.createCommunity);
+router.get('/printGeneralCommunityList', loginRequired, c_missionController.printGeneralCommunity);
+router.post('/recommendCommunity', loginRequired, c_missionController.recommendCommunity);
 
 module.exports = router;

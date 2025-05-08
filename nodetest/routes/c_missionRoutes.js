851210d378cb75ff4574e1c0d1eae84be1824d76
LeeJunMin <min73773@naver.com>
2025-05-08 13:46:30 +0900

    c_missionRoutes.js 수정 in nodetest

--- nodetest/routes/c_missionRoutes.js
+++ nodetest/routes/c_missionRoutes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const c_missionController = require('../controllers/c_missionController');
 const loginRequired = require('../middleware/loginRequired'); // JWT 기반 인증 미들웨어
+const multer = require('multer');
+// 메모리 저장소로 설정 (DB 저장용)
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 router.post('/create', loginRequired, c_missionController.createCommunityMission);
 router.post('/accept', loginRequired, c_missionController.acceptCommunityMission);
@@ -9,7 +13,7 @@ router.delete('/delete', loginRequired, c_missionController.deleteCommunityMissi
 
 router.get('/list', loginRequired, c_missionController.getCommunityMission);
 
-router.post('/createGeneralCommunity', loginRequired, c_missionController.createCommunity);
+router.post('/createGeneralCommunity', loginRequired, upload.single('image'), c_missionController.createCommunity);
 router.get('/printGeneralCommunityList', loginRequired, c_missionController.printGeneralCommunity);
 router.post('/recommendCommunity', loginRequired, c_missionController.recommendCommunity);
 

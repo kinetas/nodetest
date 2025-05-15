03166637bca23e5dd7f5e843bee3b25bca508334
병욱 조 <jbu0816@gmail.com>
2025-05-15 18:53:47 +0900

    리그라우터 lp지급 api

--- nodetest/routes/leagueRoutes.js
+++ nodetest/routes/leagueRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const leagueController = require('../controllers/leagueController');
 //배치 api
 router.post('/league/assign', leagueController.assignInitialLeague);
-//본인 리그 조회
+//본인 리그 조회 api
 router.get('/detail/:user_id', leagueController.getLeagueDetail);
+//lp 지급 api
+router.post('/mission-lp', leagueController.updateLpOnMission);
 module.exports = router;

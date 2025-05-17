0268b45837b124fca772db48f52fc9dd2de86e66
병욱 조 <jbu0816@gmail.com>
2025-05-17 14:38:32 +0900

    bpf 탐지

--- bpfdoor/trace_connect.c
+++ bpfdoor/trace_connect.c
@@ -1,10 +1,9 @@
-#include <linux/ptrace.h>
-#include <linux/bpf.h>
+#include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 
 SEC("tracepoint/syscalls/sys_enter_connect")
 int trace_connect(struct trace_event_raw_sys_enter* ctx) {
-    bpf_printk("🛡️ Connect syscall detected!\n");
+    bpf_printk("🛡️ connect() syscall detected!\n");
     return 0;
 }
 

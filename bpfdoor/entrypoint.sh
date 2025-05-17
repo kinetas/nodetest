b03bc507f43331cc8f7dd356c1b15656fc0d5a4f
병욱 조 <jbu0816@gmail.com>
2025-05-17 14:47:09 +0900

    bpf 탐지

--- bpfdoor/entrypoint.sh
+++ bpfdoor/entrypoint.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 set -e
-# 마운트 확인
-#mount -t debugfs none /sys/kernel/debug || true
-#mount -t bpffs bpffs /sys/fs/bpf || true
 
 echo "[*] Building BPF..."
 make clean && make
 
 echo "[*] Loading BPF..."
-bpftool prog load trace_connect.o /sys/fs/bpf/trace_connect type tracepoint
-bpftool prog attach name trace_connect tracepoint syscalls:sys_enter_connect
+mkdir -p /sys/fs/bpf/mybpfs
+bpftool prog load trace_connect.o /sys/fs/bpf/mybpfs/trace_connect type tracepoint
+bpftool prog attach /sys/fs/bpf/mybpfs/trace_connect tracepoint syscalls:sys_enter_connect
 
 echo "[*] Tailing logs (Press Ctrl+C to exit)"
 cat /sys/kernel/debug/tracing/trace_pipe

818404e47c59b3cfbf331d84066015e2cec1074c
병욱 조 <jbu0816@gmail.com>
2025-05-17 14:58:23 +0900

    bpf 탐지

--- bpfdoor/entrypoint.sh
+++ bpfdoor/entrypoint.sh
@@ -4,10 +4,11 @@ set -e
 echo "[*] Building BPF..."
 make clean && make
 
-echo "[*] Loading BPF program without pinning..."
-# Load the program and capture its ID
-bpftool prog load trace_connect.o /dev/null type tracepoint
-prog_id=$(bpftool prog list | grep trace_connect | awk '{print $1}' | cut -d':' -f1 | tail -n 1)
+echo "[*] Loading & Attaching BPF program without pinning..."
+# 이 명령은 pin 없이 메모리 attach + prog id 얻기
+bpftool prog loadall trace_connect.o /sys/fs/bpf_tmp type tracepoint || true
+
+prog_id=$(bpftool prog list | grep tracepoint | grep trace_connect | awk '{print $1}' | cut -d':' -f1 | tail -n 1)
 
 echo "[*] Attaching to tracepoint with prog ID: $prog_id"
 bpftool prog attach id "$prog_id" tracepoint syscalls:sys_enter_connect

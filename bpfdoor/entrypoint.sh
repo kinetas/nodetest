fba92deb3034027dfcdf680f18db34db9223448a
병욱 조 <jbu0816@gmail.com>
2025-05-17 14:53:50 +0900

    bpf 탐지

--- bpfdoor/entrypoint.sh
+++ bpfdoor/entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 echo "[*] Building BPF..."
 make clean && make
 
-echo "[*] Loading BPF (attach only)..."
-bpftool prog load trace_connect.o /sys/fs/bpf/trace_connect type tracepoint
-bpftool prog attach /sys/fs/bpf/trace_connect tracepoint syscalls:sys_enter_connect
+echo "[*] Loading BPF (no pin)..."
+bpftool prog load trace_connect.o - type tracepoint
+bpftool prog attach tracepoint syscalls:sys_enter_connect id $(bpftool prog list | grep tracepoint | awk '{print $1}' | cut -d: -f1 | tail -n 1)
 
 echo "[*] Tailing logs (Press Ctrl+C to exit)"
 cat /sys/kernel/debug/tracing/trace_pipe

3f5232dc2baaf6b9f48411245e6a9d783f545cf1
병욱 조 <jbu0816@gmail.com>
2025-05-19 20:33:13 +0900

    리그 스케줄러 테스트

--- nodetest/middleware/leagueTest.js
+++ nodetest/middleware/leagueTest.js
@@ -1,5 +1,5 @@
+require('dotenv').config();
 const db = require('../config/db');
-const { QueryTypes } = require('sequelize');
 
 const { runWeeklyLeagueEvaluation } = require('./leagueScheduler');
 runWeeklyLeagueEvaluation();

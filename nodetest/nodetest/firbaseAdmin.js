0fc0082c9619e505f9fcb183c2384385ec780d35
kinetas <kimjh7302@naver.com>
2025-05-23 18:07:54 +0900

    a

--- nodetest/nodetest/firbaseAdmin.js
+++
@@ -1,9 +0,0 @@
-//Firebase Admin SDK 초기화
-const admin = require('firebase-admin');
-const serviceAccount = require('./path/to/serviceAccountKey.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = admin;

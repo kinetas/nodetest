4600ac2b26ef898bf9817fa15ea73e2a5ccd8096
공태훈 <gth990624@gmail.com>
2025-05-25 13:22:13 +0900

    2025/05/25 경로수정 했습니다("http://27.113.11.48:3000/nodetest/~~~)

--- lib/Screens/CalendarScreen/WeeklyCalendarScreen.dart
+++ lib/Screens/CalendarScreen/WeeklyCalendarScreen.dart
@@ -228,7 +228,7 @@ class _WeeklyCalendarState extends State<WeeklyCalendar> {
   }
 
   Future<void> fetchAssignedMissions() async {
-    final url = 'http://27.113.11.48:3000/api/missions/missions/assigned';
+    final url = 'http://27.113.11.48:3000/nodetest/api/missions/missions/assigned';
 
     try {
       final response = await SessionTokenManager.get(url);

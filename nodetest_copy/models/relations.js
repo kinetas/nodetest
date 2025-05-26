8f59ac3464ad4f74bab7115ad7ddbf18edf7f4d3
LeeJunMin <min73773@naver.com>
2025-05-26 18:21:38 +0900

    nodetestcopy 삭제 in main

--- nodetest_copy/models/relations.js
+++
@@ -1,8 +0,0 @@
-const Room = require('./roomModel');
-const Mission = require('./missionModel');
-
-// Room과 Mission 간의 관계 설정
-Mission.belongsTo(Room, { foreignKey: 'r_id', as: 'room' });
-Room.hasMany(Mission, { foreignKey: 'r_id', as: 'missions' });
-
-module.exports = { Room, Mission };

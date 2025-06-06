1e8818cd8344f651ec8b036021b2593aef655253
병욱 조 <jbu0816@gmail.com>
2025-06-06 16:35:51 +0900

    상점 유저인포 라우터 연동

--- nodetest/routes/shopRoutes.js
+++ nodetest/routes/shopRoutes.js
@@ -4,5 +4,17 @@ const shopController = require('../controllers/shopController');
 
 router.get('/items', shopController.getShopItems);
 router.post('/buy', shopController.buyItem);
-
+router.get('/user-info/user-points/:user_id', async (req, res) => {
+  const { user_id } = req.params;
+  try {
+    const result = await db.query(
+      'SELECT points FROM user_points WHERE user_id = ?',
+      { replacements: [user_id], type: QueryTypes.SELECT }
+    );
+    const points = result[0]?.points || 0;
+    res.json({ points });
+  } catch (err) {
+    res.status(500).json({ error: 'DB 조회 실패' });
+  }
+});
 module.exports = router;

6a0097237667f58d7d2f0035aafed362ecf270bf
병욱 조 <jbu0816@gmail.com>
2025-06-08 17:34:53 +0900

    리그 라우터  유저정보 조회

--- nodetest/routes/leagueRoutes.js
+++ nodetest/routes/leagueRoutes.js
@@ -7,4 +7,6 @@ router.post('/assign', leagueController.assignInitialLeague);
 router.get('/detail/:user_id', leagueController.getLeagueDetail);
 //lp 지급 api
 router.post('/mission-lp', leagueController.updateLpOnMission);
+//유저 정보 조회 api
+router.get('/user-info/:user_id', leagueController.getUserInfoById);
 module.exports = router;

442c24990168bd7dc66541254fe140f27971a873
LeeJunMin <min73773@naver.com>
2025-06-15 23:59:37 +0900

    c_missionfoController.js 수정 in main nodetest

--- nodetest/controllers/c_missionController.js
+++ nodetest/controllers/c_missionController.js
@@ -548,6 +548,12 @@ exports.writeComment = async (req, res) => {
     const u_id = req.currentUserId;
 
     try {
+        // 사용자 닉네임 조회
+        const user = await User.findOne({ where: { u_id } });
+        if (!user) {
+            return res.status(404).json({ success: false, message: '사용자를 찾을 수 없습니다.' });
+        }
+
         // 댓글 생성
         await CommunityComment.create({
             cc_num: uuidv4(),
@@ -565,15 +571,15 @@ exports.writeComment = async (req, res) => {
             return res.status(404).json({ success: false, message: '커뮤니티를 찾을 수 없습니다.' });
         }
         // ================ 알림 추가 - 디바이스 토큰 =======================
-        // const sendCommentNotification = await notificationController.sendCommentNotification(
-        //     cr.u1_id,   //알림 받을 사람 = 커뮤니티 작성자
-        //     comment,
-        //     cr.cr_title
-        // );
+        const sendCommentNotification = await notificationController.sendCommentNotification(
+            cr.u1_id,   //알림 받을 사람 = 커뮤니티 작성자
+            comment,
+            cr.cr_title
+        );
 
-        // if(!sendCommentNotification){
-        //     return res.status(400).json({ success: false, message: '댓글 송신 알림 전송을 실패했습니다.' });
-        // }
+        if(!sendCommentNotification){
+            return res.status(400).json({ success: false, message: '댓글 송신 알림 전송을 실패했습니다.' });
+        }
         // ================ 알림 추가 - 디바이스 토큰 =======================
         // ==============  추가  ====================
 

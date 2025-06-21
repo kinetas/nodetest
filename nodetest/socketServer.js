9cbeee4647c1d75dec174d821da323c790204fa1
LeeJunMin <min73773@naver.com>
2025-06-22 01:36:25 +0900

    미들웨어 수정 in nodetest

--- nodetest/socketServer.js
+++ nodetest/socketServer.js
@@ -512,5 +512,3 @@ server.listen(3001, () => {
 
 // // io, sendMessage, server 내보내기
 // module.exports = { io, sendMessage, server };
-
-socket.emit('messageSent', { success: true, m_id: newMessage.m_id });
